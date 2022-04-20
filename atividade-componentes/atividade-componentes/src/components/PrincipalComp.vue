edb9650d12bde6cba43add2b7071b07c5895a8c5
viniihds <102307648+viniihds@users.noreply.github.com>
2022-04-20 20:42:23 -0300

    Add files via upload

---
+++ atividade-componentes/atividade-componentes/src/components/PrincipalComp.vue
@@ -0,0 +1,16 @@
+<script>
+import NovoBloco from "@/components/NovoBloco.vue"
+export default {
+    components: {NovoBloco},
+};
+</script>
+<template>
+    <main id="content">
+        <NovoBloco class="NovoBloco" />
+        <NovoBloco class="NovoBloco"/>
+        <NovoBloco class="NovoBloco"/>
+        <NovoBloco class="NovoBloco"/>
+        <NovoBloco class="NovoBloco"/>
+        <NovoBloco class="NovoBloco"/>
+    </main>
+</template>
